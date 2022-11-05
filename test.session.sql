1481a6904970208b543ceed1d3b58cbe51f0bea7
Co0lah <celes.thym@glaposte.net>
2022-11-05 03:55:55 +0100

    update db, added vehicle rotation handler

--- test.session.sql
+++ test.session.sql
@@ -1,9 +1,3 @@
-use mtaservver;
-create table vehicles(
-    id int auto_increment,
-    model int not null,
-    x real not null,
-    y real not null,
-    z real not null,
-    primary key(id)
-) engine = InnoDB default charset = utf8mb4;
+alter table vehicles
+add rotation_y real not null,
+    add rotation_z real not null;
--- vehicles/vehicles.lua
+++ vehicles/vehicles.lua
@@ -3,12 +3,14 @@
 
 function createVehicleForPlayer(player, command, model)
     local db = exports.db:getConnection()
-    local x, y ,z = getElementPosition(player)
+    local x, y, z = getElementPosition(player)
+    local rotX, rotY, rotZ = getElementRotation(player)
+    rotZ = rotZ + 90 -- make the vehicle driver door facing the player on spawn
     y = y + 5
 
-    dbExec(db, 'insert into vehicles(model, x, y, z) values(?,?,?,?)', model,x, y, z)
+    dbExec(db, 'INSERT INTO vehicles (model, x, y, z, rotation_x, rotation_y, rotation_z) VALUES(?,?,?,?,?,?,?)', model,x, y, z, rotX, rotY, rotZ)
 
-    local vehicleObject = createVehicle(model, x, y, z)
+    local vehicleObject = createVehicle(model, x, y, z, rotX, rotY, rotZ)
 
     dbQuery(function(queryHandle) 
         local res = dbPoll(queryHandle, 0)
@@ -17,7 +19,7 @@ function createVehicleForPlayer(player, command, model)
 
         setElementData(vehicleObject, "id", vehicle.id)
 
-    end, db, 'select id from vehicles order by id desc limit 1')
+    end, db, 'SELECT id FROM vehicles ORDER BY id DESC LIMIT 1')
 
 end
 
@@ -28,7 +30,7 @@ function loadAllVehicles(queryHandle)
     local res = dbPoll(queryHandle, 0)
 
     for index, vehicle in pairs(res) do 
-         local vehicleObject = createVehicle(vehicle.model, vehicle.x, vehicle.y, vehicle.z)
+         local vehicleObject = createVehicle(vehicle.model, vehicle.x, vehicle.y, vehicle.z, vehicle.rotation_x, vehicle.rotation_y, vehicle.rotation_z)
 -- Set vehicleObject to  specific id
         setElementData(vehicleObject, "id", vehicle.id)
     end
@@ -38,7 +40,7 @@ addEventHandler('onResourceStart', resourceRoot, function()
     local db = exports.db:getConnection()
     -- loadAllVehicles callback run when response is received from the db
 
-     dbQuery(loadAllVehicles, db, 'select * from vehicles')
+     dbQuery(loadAllVehicles, db, 'SELECT * FROM vehicles')
 end)
 
 addEventHandler('onResourceStop', resourceRoot, function()
@@ -48,10 +50,12 @@ addEventHandler('onResourceStop', resourceRoot, function()
 
     for index, vehicle in pairs(vehicles) do
     local id = getElementData(vehicle, 'id')
-    local x,y,z = getElementPosition(vehicle)
+    local x, y, z = getElementPosition(vehicle)
+    local rotX, rotY, rotZ = getElementRotation(vehicle)
 
 
-    dbExec(db, 'update vehicles set x = ?, y = ?, z = ? where id = ?', x, y, z, id)
+
+    dbExec(db, 'UPDATE vehicles SET x = ?, y = ?, z = ? , rotation_x = ?, rotation_y = ?, rotation_z = ? WHERE id = ?', x, y, z, rotX, rotY, rotZ, id)
 end
 
 end)
